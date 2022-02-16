41758b64831af251f0ac4d64687e9b18a5640056
jesseel <Jesseel@freshkart.io>
2022-02-16 19:26:48 +0530

    made changes to display

--- apipack2/openfoodfacts/trailapi.py
+++ apipack2/openfoodfacts/trailapi.py
@@ -9,7 +9,8 @@ import uvicorn
 
 
 
-#data = main_func(url)
+i=0
+l=[]
 
 app = FastAPI()
 
@@ -23,17 +24,23 @@ def main_func():
 def sec_func():
     r = requests.get('https://world.openfoodfacts.org/brands.json')
     data = r.json()
+    d = data['tags']
     df = pd.DataFrame(columns=['data_id','data_name','products','link'])
 
     for item in data['tags']:
+        global i
+        while(i<5):
+            for i in range(0,5):
+                l.append(d[i])
+                i+=1
+                        
         data_id = item['id']
         data_name = item['name']
         products = item['products']
         link = item['url']
         df=df.append({'data_id':data_id,'data_name':data_name ,'products':products,'link':link}, ignore_index=True)
         df3= df.to_csv("foodfacts.csv",index=False)
-    jsondata = json.dumps(data,indent= 2)
-    return(jsondata)
+    return(l)
 
 
 
